a24843d34b97d2cb82d32a38579bb3f9231a58cc
Ainkub <aleksandr.2000.ru@yandex.ru>
2023-08-25 09:56:53 +0300

    new: added connection to db and insert historical rates into db edit: removed app from docker-compose (inefficient) new: added requirements

--- script.py
+++ script.py
@@ -1,9 +1,41 @@
-from typing import List
+from typing import Dict, List
 
 import psycopg2
 import requests
 
 from config import DATABASE, HOST, PASSWORD, PORT, TIMESERIES_URL, USERNAME
+
+
+def create_historical_rates_table(connection) -> None:
+    query = ('CREATE TABLE IF NOT EXISTS historical_rates ('
+             'id serial4 PRIMARY KEY, '
+             '"date" DATE NOT NULL, '
+             'first_currency VARCHAR(3) NOT NULL, '
+             'second_currency VARCHAR(3) NOT NULL, '
+             'rate REAL NOT NULL)')
+    
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+
+
+def insert_historical_rates_to_db(connection,
+                                  base_currency: str,
+                                  historical_rates: Dict[str, Dict[str, str]]):
+    
+    query = ('INSERT INTO historical_rates ("date", first_currency, second_currency, rate) '
+             'VALUES (%s, %s, %s, %s)')
+    
+    data = []
+    for date, rate in historical_rates.items():
+        second_currency, rate = list(rate.items())[0]
+        data.append((date, base_currency, second_currency, rate))
+
+    cursor = connection.cursor()
+    cursor.executemany(query, data)
+    connection.commit()
+    cursor.close()    
 
 
 def get_historical_rates_data(start_date: str, 
@@ -49,3 +81,9 @@ if __name__ == '__main__':
                                   user=USERNAME,
                                   password=PASSWORD,
                                   database=DATABASE)
+    
+    create_historical_rates_table(connection)
+    insert_historical_rates_to_db(connection,
+                                  base_currency='RUB',
+                                  historical_rates=historical_rates_data['rates'])
+    connection.close()
